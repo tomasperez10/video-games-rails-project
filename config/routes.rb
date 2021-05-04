b9e8dc03525743810d2af877dd5df6829febff5a
Tomas Perez <tomasitopg6@gmail.com>
2021-05-04 04:07:04 -0400

    added basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,16 @@ Rails.application.routes.draw do
   resources :genres
   resources :video_games
   resources :users
+
+  post '/video_games/new', to: 'video_games#index'
+
+  get '/auth/google/callback', to: 'users#google_login'
+
+  get '/login', to: 'sessions#new'
+
+  post '/login', to: 'sessions#create'
+
+  get '/logout', to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
