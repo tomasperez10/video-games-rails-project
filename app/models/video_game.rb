7efb025674572e0a32118bbd148ae9f06fcee5a2
Tomas Perez <tomasitopg6@gmail.com>
2021-05-05 02:30:30 -0400

    added genre_attributes method to video game model

--- app/models/video_game.rb
+++ app/models/video_game.rb
@@ -6,4 +6,12 @@ class VideoGame < ApplicationRecord
   validates :title, presence: true
   validates :user_happiness_score, presence: true, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 10}
 
+  def genres_attributes=(genre_attributes)
+    genre_name = genre_attributes["0"]["name"]
+    unless genre_name.blank?
+      genre = Genre.find_or_create_by(name: genre_name)
+      self.genres << genre
+    end
+  end
+
 end
