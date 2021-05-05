fe7d8dd504f7deb5d92c7f83dc776f4862572724
Tomas Perez <tomasitopg6@gmail.com>
2021-05-05 02:05:02 -0400

    added before actions and helper method to videogamescontroller

--- app/controllers/video_games_controller.rb
+++ app/controllers/video_games_controller.rb
@@ -1,2 +1,7 @@
 class VideoGamesController < ApplicationController
+  before_action :require_login
+  before_action :set_show, only: [:show, :edit, :update]
+  before_action :created_by_current_user, only: [:edit, :update]
+  helper_method :current_user
+
 end
