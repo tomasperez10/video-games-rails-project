d11ff559666254c7e57b7838a79f77f9f338b5b2
Tomas Perez <tomasitopg6@gmail.com>
2021-05-05 02:06:17 -0400

    index method for videogamescontroller

--- app/controllers/video_games_controller.rb
+++ app/controllers/video_games_controller.rb
@@ -4,4 +4,13 @@ class VideoGamesController < ApplicationController
   before_action :created_by_current_user, only: [:edit, :update]
   helper_method :current_user
 
+  def index
+    @user = User.find_by(id: params[:user_id])
+    if @user
+      @video_games = @user.video_games
+    else
+      @video_games = VideoGames.all
+    end
+  end
+
 end
