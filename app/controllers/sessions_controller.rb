ecb639116b49d25002f86e58cf4d0c4e7d0262eb
Tomas Perez <tomasitopg6@gmail.com>
2021-05-05 01:12:13 -0400

    new and create methods for sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
     helper_method :logged_in?, :current_user
     
+    def new
+        if logged_in?
+          redirect_to user_video_games_path(current_user)
+        end
+    end
+    
+    def create
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+          session[:user_id] = user.id
+          redirect_to user_video_games_path(user)
+        else
+          flash[:danger] = "Error: email and/or password is incorrect or not available"
+          render :new
+        end
+    end
 end
