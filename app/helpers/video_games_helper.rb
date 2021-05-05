a3ac0107d9ba3f61c73e5d0be4a918ecb4a3b1ab
Tomas Perez <tomasitopg6@gmail.com>
2021-05-05 03:12:46 -0400

    fixed formatting and other small detail changes

--- app/helpers/video_games_helper.rb
+++ app/helpers/video_games_helper.rb
@@ -1,7 +1,7 @@
 module VideoGamesHelper
     def genre_options(genres, video_games)
         if genres.count > 0
-          render :partial => "genres/details", :locals => {:genres => genres, :video_games => video_games}
+          render :partial => "genres/index", :locals => {:genres => genres, :video_games => video_games}
         else
           "No genres found"
         end
