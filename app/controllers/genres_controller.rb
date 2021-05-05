cc2f086108712914858a0ca6c246a293e06e8121
Tomas Perez <tomasitopg6@gmail.com>
2021-05-05 01:54:50 -0400

    methods and before_action for genrescontroller

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,12 @@
 class GenresController < ApplicationController
+    before_action :require_login
+
+    def index
+        @genres = Genre.all
+    end
+
+    def show
+        @genre = Genre.find_by(id: params[:id])
+        @video_games = @genre.video_games
+    end
 end
