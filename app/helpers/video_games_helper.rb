72e23d372f5a593f29351ec09c7ad7e576dcb7eb
Tomas Perez <tomasitopg6@gmail.com>
2021-05-05 03:07:51 -0400

    added helper for video games

--- app/helpers/video_games_helper.rb
+++ app/helpers/video_games_helper.rb
@@ -1,2 +1,9 @@
 module VideoGamesHelper
+    def genre_options(genres, video_games)
+        if genres.count > 0
+          render :partial => "genres/details", :locals => {:genres => genres, :video_games => video_games}
+        else
+          "No genres found"
+        end
+    end
 end
