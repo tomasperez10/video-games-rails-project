1dc119150c5867a401111914c85bc50ac0407cf1
Tomas Perez <tomasitopg6@gmail.com>
2021-05-05 02:03:30 -0400

    added private methods to userscontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,4 +51,14 @@ class UsersController < ApplicationController
           render :new
         end
     end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :username, :password, :password_confirmation)
+    end
+
+    def auth
+        request.env['omniauth.auth']
+    end
 end
