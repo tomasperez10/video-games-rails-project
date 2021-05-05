ebe526e84ddb065176fdf6cf0f39a8078d7df1a5
Tomas Perez <tomasitopg6@gmail.com>
2021-05-05 01:59:15 -0400

    create method for userscontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,14 @@ class UsersController < ApplicationController
             @user = User.new
         end
     end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+          session[:user_id] = @user.id
+          redirect_to user_video_games_path(@user)
+        else
+          render :new
+        end
+    end
 end
