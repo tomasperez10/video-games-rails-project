19dc1da3f9c524c6d3c135e1c149b68d7fdb686c
Tomas Perez <tomasitopg6@gmail.com>
2021-05-05 01:00:04 -0400

    made logged_in and require login method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,16 @@ class ApplicationController < ActionController::Base
         @user = User.find_by(id: session[:user_id])
     end
 
+    def logged_in?
+        !!current_user
+    end
+    
+    def require_login
+        unless logged_in?
+          flash[:danger] = "Error: you must be logged in to access this page"
+          redirect_to login_path
+        end
+    end
+
 
 end
